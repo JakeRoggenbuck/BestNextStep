a597aab28dbcc8d26eb6e4618b672d23b7cfe2da
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-06-02 13:21:04 -0700

    Add all

---
+++ daft/README.md
@@ -0,0 +1,1 @@
+# Daft - Best Next Step Backend
--- daft/daft_backend/daft_backend/asgi.py
+++ daft/daft_backend/daft_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for BestNextStep project.
+ASGI config for daft_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BestNextStep.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'daft_backend.settings')
 
 application = get_asgi_application()
