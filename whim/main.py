5184ec59ec3a7bb1aabbafbfa5fb5c16eb763348
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-07-10 10:09:37 -0700

    Add requests in python
    - Python get requests work but they do not in js right now

---
+++ whim/main.py
@@ -0,0 +1,17 @@
+import requests
+from base64 import b64encode
+from pprint import pprint
+import json
+
+home = requests.get("http://localhost:1357/")
+print(home.text)
+
+user_and_pass = b64encode(b"Admin:banana").decode("ascii")
+headers = {"Authorization": f"Basic {user_and_pass}"}
+
+home = requests.get("http://localhost:1357/api/v1", headers=headers)
+print(home.text)
+
+home = requests.get("http://localhost:1357/api/v1/all", headers=headers)
+out = home.json()["message"]
+pprint(json.loads(out))
