c349748b54d58620af7cad7cf8b13c9d002286af
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-07-12 00:36:47 -0700

    Add better ui

--- impulse/impulse_frontend/src/App.svelte
+++ impulse/impulse_frontend/src/App.svelte
@@ -1,7 +1,8 @@
 <script>
   import { onMount } from "svelte";
 
-  let reminders;
+  let steps;
+
   onMount(async () => {
     await fetch(`http://127.0.0.1:1357/api/v1/step/`, {
       method: "GET",
@@ -12,22 +13,36 @@
     })
       .then((r) => r.json())
       .then((data) => {
-        console.log(data);
+        steps = JSON.parse(data["message"]);
       });
   });
-
-  console.log(reminders);
 </script>
 
 <main>
-  <h1>Hello {reminders}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  <h1>Best Next Step</h1>
+  <div align="center">
+    {#if steps}
+      {#each steps as step}
+        <div class="step">
+          <h2>{step._id}. {step.name}</h2>
+          <h4>{step.desc}</h4>
+          <h6>from collection {step.collection}</h6>
+        </div>
+      {/each}
+    {:else}
+      <p class="loading">loading...</p>
+    {/if}
+  </div>
 </main>
 
 <style>
+  .step {
+    background-color: #f5eef8;
+    width: 50%;
+    margin: 8px;
+    padding: 2px;
+  }
+
   main {
     text-align: center;
     padding: 1em;
@@ -36,10 +51,10 @@
   }
 
   h1 {
-    color: #ff3e00;
+    color: #9e8eff;
     text-transform: uppercase;
     font-size: 4em;
-    font-weight: 100;
+    font-weight: 300;
   }
 
   @media (min-width: 640px) {
