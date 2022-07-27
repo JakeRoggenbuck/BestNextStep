bc071d98e157e960f8055cee45701792f701af12
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-07-26 18:38:21 -0700

    Add icons

--- impulse/impulse_frontend/src/Step.svelte
+++ impulse/impulse_frontend/src/Step.svelte
@@ -1,9 +1,26 @@
 <script>
   import "./app.css";
-  export let step;
+	  export let step;
+	  import { Trash2Icon, EditIcon, ArrowUpIcon, ArrowDownIcon } from 'svelte-feather-icons';
 </script>
 
 <div class="step flat">
+
+  <div class="up" on:click={() => alert("up")}>
+	<ArrowUpIcon size="24" />
+  </div>
+
+  <div class="down" on:click={() => alert("down")}>
+	<ArrowDownIcon size="24" />
+  </div>
+
+  <div class="delete" on:click={() => alert("delete")}>
+	<Trash2Icon size="24" />
+  </div>
+
+  <div class="edit" on:click={() => alert("edit")}>
+	<EditIcon size="24" />
+  </div>
   <h3>{step._id}. {step.name}</h3>
-  <p>{step.desc}</p>
+  <p class="desc">{step.desc}</p>
 </div>
