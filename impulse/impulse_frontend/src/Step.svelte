bed311ca35b83e9969266c722faddbea91644177
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-07-26 21:08:13 -0700

    Checkpoint two, add checks

--- impulse/impulse_frontend/src/Step.svelte
+++ impulse/impulse_frontend/src/Step.svelte
@@ -6,14 +6,20 @@
     EditIcon,
     ArrowUpIcon,
     ArrowDownIcon,
-	  } from "svelte-feather-icons";
+    SquareIcon,
+    CheckSquareIcon,
+  } from "svelte-feather-icons";
 
-	  async function deleteStep(step_id) {
+  function toggleDone() {
+    step.done = !step.done;
+  }
+
+  async function deleteStep(step_id) {
     await fetch(`http://127.0.0.1:1357/api/v1/step/` + step_id + "/", {
       method: "DELETE",
       headers: {
         // Test Passowrd for running locally - not used in production
-		Authorization: "Basic " + btoa("Admin:banana"),
+        Authorization: "Basic " + btoa("Admin:banana"),
       },
     })
       .then((r) => r.json())
@@ -42,6 +48,15 @@
   <div class="edit" on:click={() => alert("edit")}>
     <EditIcon size="24" />
   </div>
+
+  <div class="done" on:click={() => toggleDone()}>
+    {#if step.done}
+      <CheckSquareIcon class="checked" size="24" />
+    {:else}
+      <SquareIcon class="unchecked" size="24" />
+    {/if}
+  </div>
+
   <h3>{step._id}. {step.name}</h3>
   <p class="desc">{step.desc}</p>
 </div>
